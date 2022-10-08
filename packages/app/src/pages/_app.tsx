405b7ad25b4d74b5251400ac37ae62c05dd61987
taijusanagi <taijusanagi@gmail.com>
2022-10-09 00:36:35 +0900

    remove auth

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -3,8 +3,6 @@ import "@fontsource/inter/variable.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { RainbowKitSiweNextAuthProvider } from "@rainbow-me/rainbowkit-siwe-next-auth";
-import { SessionProvider } from "next-auth/react";
 import { WagmiConfig } from "wagmi";
 
 import { myChakraUITheme, myRainbowKitTheme } from "@/lib/theme";
@@ -14,13 +12,9 @@ const MyApp = ({ Component, pageProps }: any) => {
   return (
     <ChakraProvider resetCSS theme={myChakraUITheme}>
       <WagmiConfig client={wagmiClient}>
-        <SessionProvider refetchInterval={0} session={pageProps.session}>
-          <RainbowKitSiweNextAuthProvider>
-            <RainbowKitProvider chains={chains} showRecentTransactions={true} theme={myRainbowKitTheme}>
-              <Component {...pageProps} />
-            </RainbowKitProvider>
-          </RainbowKitSiweNextAuthProvider>
-        </SessionProvider>
+        <RainbowKitProvider chains={chains} showRecentTransactions={true} theme={myRainbowKitTheme}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
       </WagmiConfig>
     </ChakraProvider>
   );
