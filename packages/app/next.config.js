ff536a8b42ea3c1386e7e39bdf5c610b11c0943b
taijusanagi <taijusanagi@gmail.com>
2022-10-09 00:42:27 +0900

    not required env

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,8 +1,4 @@
 const nextConfig = {
-  env: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-  },
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
